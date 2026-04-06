aabd0454056d510d04ad1b00bfb630dbd06e07e8
Justin Fan <justinfan@ucsb.edu>
2026-04-06 09:58:34 -0700

    xy - updated with actual developer name

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -20,7 +20,7 @@ public class Developer {
         // You may use just the name that is used on <https://bit.ly/cs156-s26-teams>
         // i.e. your first name, or your first and initial of last name
 
-        return "Chris G.";
+        return "Justin";
     }
 
     /**
@@ -30,7 +30,7 @@ public class Developer {
 
     public static String getGithubId() {
         // TODO: Change this to your github id
-        return "cgaucho";
+        return "just-in-fan";
     }
 
     /**
@@ -40,13 +40,13 @@ public class Developer {
     
     public static Team getTeam() {
         // TODO: Change this to your team name
-        Team team = new Team("s26-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("s26-02");
+        team.addMember("Justin");
+        team.addMember("Andrew B.");
+        team.addMember("Arman");
+        team.addMember("David Chen");
+        team.addMember("Kai");
+        team.addMember("Kevin");
         return team;
     }
 }
